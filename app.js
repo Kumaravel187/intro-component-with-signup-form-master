7b4aaf842e26ffd2c1749590b35d9b9a60855b8b
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-04 19:11:29 +0800

    Completed multiple modifications:
    - Refactored code in app.js
    - Added simple email validation
    - Implemented validation for other input fields
    - Modified styling
    - Updated index.html

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const formEl = document.getElementById("form");
 const firstNameEl = document.getElementById("firstName");
+const lastNameEl = document.getElementById("lastName");
+const emailEl = document.getElementById("email");
+const passwordEl = document.getElementById("password");
 
 function showErrorMessage(input, message) {
   const parentEl = input.parentElement;
+
   const error = parentEl.querySelector(".input-message");
   const inputEl = parentEl.querySelector("input");
   inputEl.classList.add("error");
@@ -12,18 +16,42 @@ function showErrorMessage(input, message) {
 
 function showSuccessMessage(input) {
   const parentEl = input.parentElement;
+
   const success = parentEl.querySelector(".input-message");
   const inputEl = parentEl.querySelector("input");
   success.style.visibility = "hidden";
   inputEl.classList.add("success");
 }
 
+function checkRequiredField(inputElArry) {
+  inputElArry.forEach(function (inputEl) {
+    if (inputEl.value.trim() === "") {
+      showErrorMessage(
+        inputEl,
+        `Please type in your ${getInputElName(inputEl)}`
+      );
+    } else {
+      showSuccessMessage(inputEl);
+    }
+  });
+}
+
+function getInputElName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (re.test(email.value)) {
+    showSuccessMessage(email);
+  } else {
+    showErrorMessage(email, `Please use a valid email`);
+  }
+}
+
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (firstNameEl.value === "") {
-    showErrorMessage(firstNameEl, "This first name is not allowed");
-  } else {
-    showSuccessMessage(firstNameEl);
-  }
+  checkRequiredField([firstNameEl, lastNameEl, passwordEl]);
+  validateEmail(emailEl);
 });
