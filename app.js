df2cdfed37b125a3afec9cea25e0603d86a95751
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-04 14:07:16 +0800

    Complete error message display and add desktop styling for form
    - Implemented the functionality to display error messages for the form.
    - Also, introduced styling changes specifically tailored for the desktop version of the page.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const formEl = document.getElementById("form");
 const firstNameEl = document.getElementById("firstName");
 
+function showErrorMessage(input, message) {
+  const parentEl = input.parentElement;
+  const error = parentEl.querySelector(".input-message");
+  error.style.visibility = "visible";
+  error.textContent = message;
+}
+
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
   if (firstNameEl.value === "") {
-    showErrorMessage(input);
+    showErrorMessage(firstNameEl, "This first name is not allowed");
   } else {
     showSuccessMessage();
   }
